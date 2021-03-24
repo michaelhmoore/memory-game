e3953fa244c4816f1b1090ad828327b036b5136e
michael moore <michael.moore@hey.com>
2021-03-23 21:22:33 -0400

    updated app.js, app.css, and created memorycard component and css

---
+++ src/components/MemoryCard.js
@@ -0,0 +1,11 @@
+import './MemoryCard.css'
+import React from 'react';
+
+class MemoryCard extends React.Component {
+    render() {
+        return <div className="MemoryCard"></div>
+    }
+}
+
+
+export default MemoryCard;
